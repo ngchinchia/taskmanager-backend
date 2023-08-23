8b8df5e6bb8b10308a8b2a0c59c9dfd71f405bbf
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-23 16:06:06 +0800

    install nodemon & ts-node

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 // Instantiate express app
 const app: Express = express();
+dotenv.config()
 
 // Define server port
-const port = 3200;
+const port = process.env.PORT;
 
 // Create default route
 app.get("/", (req: Request, res: Response) => {
