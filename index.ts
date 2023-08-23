bea72fccf1ce318051f2873c8b847a8cf54838b0
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-23 17:46:53 +0800

    add cors & body-parser

--- index.ts
+++ index.ts
@@ -1,17 +1,43 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, {
+  Express,
+  Request,
+  Response,
+} from 'express';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 // Instantiate express app
 const app: Express = express();
-dotenv.config()
+dotenv.config();
+
+// Create Database Connection
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true, // Create tables in database and delete tables that is removed from our code as well
+});
 
 // Define server port
 const port = process.env.PORT;
 
 // Create default route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + Typescript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + Typescript Server');
 });
 
-// Start listening to request
-app.listen(port);
+AppDataSource.initialize()
+  .then(() => {
+    // Start listening to request
+    app.listen(port);
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error(
+      'Error during Data Source initialization',
+      err,
+    );
+  });
