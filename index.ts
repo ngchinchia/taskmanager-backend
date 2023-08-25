5c5f84e4940cb0fbd6d14bd6cb10d3e5289dbd14
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-25 12:37:22 +0800

    add sqlhost

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ app.use(cors());
 // Create Database Connection
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.MYSQL_HOST,
   port: 3306,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
