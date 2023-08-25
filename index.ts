4444b02897f823f17bca7793726b3f319e2a8507
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-25 14:50:20 +0800

    Add connection URI to cloud db

--- index.ts
+++ index.ts
@@ -29,12 +29,9 @@ app.use(cors());
 // Create Database Connection
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.MYSQL_HOST,
-  port: 3306,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-  entities: [Task],
+  url: process.env.DATABASE_URI,
+  logging: false, 
+  entities: [Task], 
   synchronize: true, // Create tables in database and delete tables that is removed from our code as well
 });
 
